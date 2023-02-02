6580de3a29d00dfda9f21d6674cde257e8bcea1b
MxCabrera <migueledward@hotmail.com>
2023-02-01 21:23:59 -0500

    added script tags to the contact and blog page, now hamburger menu works correctly on all html files

--- script.js
+++ script.js
@@ -7,4 +7,4 @@ const navMenu = document.querySelector('.nav-menu');
 hamburger.addEventListener('click', () => {
    hamburger.classList.toggle('active');
    navMenu.classList.toggle('active');
-})
+});
