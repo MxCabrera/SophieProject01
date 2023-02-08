738237511a001a42df35309d7ebb3c0bb99b004f
MxCabrera <migueledward@hotmail.com>
2023-02-08 16:15:26 -0500

    created a small error handle incase users do not fill out form properly

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const myForm = document.querySelector('.formBox');
 const input = document.querySelector('input');
-const label = document.querySelector('label');
+const email = document.querySelector('.email');
+const message = document.querySelector('.message');
 const submitInput = document.querySelector('.submit');
 
-console.log(submitInput);
+// console.log(input);
 
 // ERROR HANDLING.
 
@@ -11,11 +12,30 @@ console.log(submitInput);
 
 //
 
-
-
-submitInput.addEventListener('click', function(e){
-   e.preventDefault()
-   console.log('i was able to submit something!');
+myForm.addEventListener('submit', function(e){
+   console.log('yoooo');
+   
+   // if any of the fields are empty, send out alert. else, accept the information?
+   if (input.value == '') {
+      //prevents submission if any fields are empty strings
+      e.preventDefault();
+      alert('do not leave the name field empty');
+      return false;
+   } else if (email.value == '') {
+      e.preventDefault();
+      alert('do not leave the email empty');
+      return false;
+   } else if (message.value == '') {
+      e.preventDefault();
+      alert('do not leave the message field empty');
+   } else {
+      alert('accepted');
+   }
 });
+
+// submitInput.addEventListener('click', function(e){
+//    e.preventDefault()
+//    console.log('i was able to submit somethings!');
+// });
 
 
