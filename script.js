6fb6c30cc0a563525200ef1eb42c75f2df892702
MxCabrera <migueledward@hotmail.com>
2023-02-09 19:22:07 -0500

    created a text area instead of input tag for the main message, character limit as well as a counter that listens to user input

--- script.js
+++ script.js
@@ -1,41 +1,55 @@
+// store specific classes/elements into a variable using querySelector. 
 const myForm = document.querySelector('.formBox');
 const input = document.querySelector('input');
 const email = document.querySelector('.email');
 const message = document.querySelector('.message');
-const submitInput = document.querySelector('.submit');
-
-// console.log(input);
 
 // ERROR HANDLING.
 
 // user will add their email, if the input for their name/email/and message is not equal to an empty string, do proceed. else if it is, dont proceed with the submit
 
-//
-
 myForm.addEventListener('submit', function(e){
    console.log('yoooo');
    
-   // if any of the fields are empty, send out alert. else, accept the information?
+   // if any of the fields are empty, send out alert. else, accept the information.
    if (input.value == '') {
       //prevents submission if any fields are empty strings
       e.preventDefault();
       alert('do not leave the name field empty');
-      return false;
    } else if (email.value == '') {
       e.preventDefault();
       alert('do not leave the email empty');
-      return false;
    } else if (message.value == '') {
       e.preventDefault();
       alert('do not leave the message field empty');
    } else {
-      alert('accepted');
+      console.log('accepted!');
    }
 });
 
-// submitInput.addEventListener('click', function(e){
-//    e.preventDefault()
-//    console.log('i was able to submit somethings!');
-// });
+// creating a character counter for the message limit of 200 characters. 
 
+// storing id tags into variables using getElementById.
+let textArea = document.getElementById('message');
+let characterCount = document.getElementById('charCount');
+const maxCharacters = 200;
 
+// function that is called when a user inputs a character
+const countCharacters = function () {
+   
+   let characterUserEntered = textArea.value.length;
+   let counter = maxCharacters - characterUserEntered;
+   characterCount.textContent = counter + "/200";
+
+   // if statement that says if user reaches a certain character count, the entire span will change color. 
+   if (counter <= 0) {
+      characterCount.style.color = 'red';
+   } else if (counter < 40) {
+      characterCount.style.color = 'orange';
+   } else {
+      characterCount.style.color = 'black';
+   };
+};
+
+// event listener that is looking for a user input, which will then call countCharacter function. 
+textArea.addEventListener('input', countCharacters);
